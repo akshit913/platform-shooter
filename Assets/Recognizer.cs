907c48dc01293300eb3daa8fecc90d6345e761b4
willengel4 <willengel4@gmail.com>
2020-02-21 16:29:17 -0500

    player ground and movement

---
+++ Assets/Recognizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recognizer : MonoBehaviour
+{
+    public Vector3 hiddenPosition;
+    public GameObject player;
+    public GameObject tap;
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            temp.z = transform.position.z;
+            transform.position = temp;
+
+            if(Mathf.Abs(transform.position.x - tap.transform.position.x) > 0.5f)
+            {
+                if(transform.position.x > tap.transform.position.x)
+                    player.transform.position += new Vector3(0.03f,0,0);
+                else
+                    player.transform.position -= new Vector3(0.03f,0,0);
+            }
+
+
+        }
+        else 
+        {
+            transform.position = hiddenPosition;
+        }
+    }
+}
---
+++ Assets/Tap.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour
+{
+    private Vector3 position;
+    private float width;
+    private float height;
+    public Vector3 hiddenPosition;
+
+    void Awake()
+    {
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+
+        // Position used for the cube.
+        position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    void OnGUI()
+    {
+        // Compute a fontSize based on the size of the screen width.
+        GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            temp.z = position.z;
+            position = temp;
+            transform.position = position;
+        }
+        else if(Input.GetMouseButton(0))
+        {
+
+        }
+        else 
+        {
+            position = hiddenPosition;
+            transform.position = position;
+        }
+    }
+}
