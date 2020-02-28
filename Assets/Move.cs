083ff3864f34da0c09efdd3cb22e03e0f6a9b8ae
Will Engel <willengel4@gmail.com>
2020-02-28 12:45:55 -0500

    divided shooting and movement, added enemy, basic shooting.

---
+++ Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body.AddForce(Vector3.right * 100.0f);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- Assets/Recognizer.cs
+++ Assets/Recognizer.cs
@@ -8,31 +8,94 @@ public class Recognizer : MonoBehaviour
     public GameObject player;
     public GameObject tap;
     public Rigidbody playerRigidBody;
+    public Texture tapDownTexture;
+    public Texture movementHalf;
+    public Texture shootingHalf;
+    private Vector3 tapDownPoint;
+    private Vector3 tapDragPoint;
+    public float movementSense;
+    public float jumpSense;
+    public GameObject bullet;
 
     void Awake()
     {
+        tapDragPoint = hiddenPosition;
+    }
 
+    void OnGUI () 
+    {
+        GUI.Box(new Rect(0, 0, Screen.width, Screen.height / 2), shootingHalf);
+
+        if(tapDownPoint != hiddenPosition) 
+        {
+            GUI.Box(new Rect(tapDownPoint.x - 10, Screen.height - tapDownPoint.y - 10, 20, 20), tapDownTexture);
+        }
     }
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        /* Initial tap */
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            temp.z = transform.position.z;
-            transform.position = temp;
+            Vector3 temp = Input.mousePosition;
 
-            if(Mathf.Abs(transform.position.x - tap.transform.position.x) > 0.5f)
+            /* Lower half = movement gesture */
+            if(temp.y < Screen.height / 2)
             {
-                if(transform.position.x > tap.transform.position.x)
-                    player.transform.position += new Vector3(0.03f,0,0);
-                else
-                    player.transform.position -= new Vector3(0.03f,0,0);
+                tapDownPoint = temp;
+            }
+
+            /* Upper half = shooting gesture */
+            else
+            {
+                if( Mathf.Abs(temp.x - Screen.width / 2) > 20 && Mathf.Abs(temp.y - Screen.height / 2) > 20)
+                {
+                    Instantiate(bullet, new Vector3(player.transform.position.x, player.transform.position.y + 1, 1), new Quaternion(0, 0, 0, 1));
+                }
+
             }
         }
+
+        /* Drag */
+        else if (Input.GetMouseButton(0))
+        {
+            if(tapDownPoint != hiddenPosition)
+            {
+                tapDragPoint = Input.mousePosition;
+
+                if(Mathf.Abs(tapDragPoint.x - tapDownPoint.x) > movementSense)
+                {
+                    if(tapDragPoint.x > tapDownPoint.x)
+                        player.transform.position += new Vector3(0.06f,0,0);
+                    else
+                        player.transform.position -= new Vector3(0.06f,0,0);
+                }
+            }
+        }
+
+        /* No tap */
         else 
         {
-            transform.position = hiddenPosition;
+            if(tapDownPoint != hiddenPosition)
+            {
+                if(tapDragPoint.y - tapDownPoint.y > jumpSense)
+                {
+                    if(tapDragPoint.x - tapDownPoint.x > jumpSense)
+                        playerRigidBody.AddForce(Vector3.right * 100.0f);
+                    if(tapDownPoint.x - tapDragPoint.x > jumpSense)
+                        playerRigidBody.AddForce(Vector3.left * 100.0f);
+
+                    playerRigidBody.AddForce(Vector3.up * 500.0f);
+                }
+                
+                if(tapDownPoint.y - tapDragPoint.y > jumpSense)
+                {
+                    playerRigidBody.AddForce(Vector3.down * 500.0f);
+                }
+
+                tapDragPoint = hiddenPosition;
+                tapDownPoint = hiddenPosition;
+            }
         }
     }
 }
